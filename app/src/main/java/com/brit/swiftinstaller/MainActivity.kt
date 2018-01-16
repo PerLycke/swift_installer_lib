329856dc21bc15788cf7eddd39877549a3cd1295
Per Lycke <per.lycke@gmail.com>
2018-01-16 16:19:07 +0100

    Initial commit

---
+++ app/src/main/java/com/brit/swiftinstaller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.brit.swiftinstaller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myToolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
+        setSupportActionBar(myToolbar)
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE)
+        supportActionBar?.setTitle(R.string.app_name)
+//        supportActionBar?.setSubtitle("0 / 200 overlays ic_install")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
