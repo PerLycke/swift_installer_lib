026d545bf194a0d1bc290aeafe7f10db2e883a55
PerLycke <per.lycke@gmail.com>
2018-04-10 17:18:21 +0200

    add result activity

---
+++ app/src/main/java/com/brit/swiftinstaller/ui/activities/ResultActivity.kt
@@ -0,0 +1,13 @@
+package com.brit.swiftinstaller.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.brit.swiftinstaller.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
