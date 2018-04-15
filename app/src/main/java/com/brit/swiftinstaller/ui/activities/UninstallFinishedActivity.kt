94170dbaf50122484f93d173971f8afad944939e
PerLycke <per.lycke@gmail.com>
2018-04-15 19:26:41 +0200

    show reboot dialog after uninstall finishes

--- app/src/main/java/com/brit/swiftinstaller/ui/activities/UninstallFinishedActivity.kt
+++ app/src/main/java/com/brit/swiftinstaller/ui/activities/UninstallFinishedActivity.kt
@@ -2,44 +2,42 @@ package com.brit.swiftinstaller.ui.activities
 
 import android.os.Bundle
 import android.os.PowerManager
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDelegate
 import android.view.View
 import com.brit.swiftinstaller.R
 import com.brit.swiftinstaller.ui.ThemedBottomSheetDialog
+import kotlinx.android.synthetic.main.alert_dialog_uninstall_reboot.view.*
 import kotlinx.android.synthetic.main.sheet_install_summary_fab.view.*
 
 class UninstallFinishedActivity: AppCompatActivity() {
 
-    lateinit var bottomSheetDialog: ThemedBottomSheetDialog
+    lateinit var dialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bottomSheetDialog = ThemedBottomSheetDialog(this)
-        val sheetView = View.inflate(this, R.layout.sheet_install_summary_fab, null)
 
-        sheetView.sendLog.visibility = View.GONE
-
-        sheetView.reboot.setOnClickListener {
-            val pm = getSystemService(PowerManager::class.java)
-            bottomSheetDialog.dismiss()
-            pm.reboot(null)
+        val inflate = View.inflate(this, R.layout.alert_dialog_uninstall_reboot, null)
+        val builder: AlertDialog.Builder
+        if (AppCompatDelegate.getDefaultNightMode()
+                == AppCompatDelegate.MODE_NIGHT_YES) {
+            builder = AlertDialog.Builder(this, R.style.AppAlertDialogTheme_Black)
+        } else {
+            builder = AlertDialog.Builder(this, R.style.AppAlertDialogTheme)
         }
+        builder.setView(inflate)
+        dialog = builder.create()
 
-        sheetView.rebootLater.setOnClickListener {
-            bottomSheetDialog.dismiss()
-            finish()
-        }
-
-        bottomSheetDialog.setContentView(sheetView)
-        bottomSheetDialog.setOnCancelListener {
-            finish()
+        inflate.rebootUninstallBtn.setOnClickListener {
+            dialog.dismiss()
         }
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
-            bottomSheetDialog.show()
+            dialog.show()
         }
     }
 }
