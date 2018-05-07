0424083f7e2b011edd5c70ff400a6446aa4dd448
PerLycke <per.lycke@gmail.com>
2018-05-07 17:25:20 +0200

    fix delete chars

--- app/src/main/java/com/brit/swiftinstaller/ui/activities/CustomizeActivity.kt
+++ app/src/main/java/com/brit/swiftinstaller/ui/activities/CustomizeActivity.kt
@@ -148,8 +148,6 @@ class CustomizeActivity : ThemeActivity() {
 
         accent_hex_input.onFocusChangeListener = onFocusChangeListener
         hex_input_bg.onFocusChangeListener = onFocusChangeListener
-        accent_hex_input.setOnKeyListener(keyListener)
-        hex_input_bg.setOnKeyListener(keyListener)
 
         accent_hex_input.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
