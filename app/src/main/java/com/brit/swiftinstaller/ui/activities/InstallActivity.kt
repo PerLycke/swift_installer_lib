3a1168904c91712831d6b2e4fbc2e2044bcba4e6
PerLycke <per.lycke@gmail.com>
2018-06-08 22:38:31 +0200

    Revert: added runnables for dialogs

--- app/src/main/java/com/brit/swiftinstaller/ui/activities/InstallActivity.kt
+++ app/src/main/java/com/brit/swiftinstaller/ui/activities/InstallActivity.kt
@@ -30,7 +30,6 @@ class InstallActivity : ThemeActivity() {
     private lateinit var progressCount: TextView
     private lateinit var progressPercent: TextView
     private val installListener = InstallListener()
-    private val mHandler = Handler()
 
     private var uninstall = false
     private var update = false
@@ -127,7 +126,7 @@ class InstallActivity : ThemeActivity() {
             progressCount.visibility = View.INVISIBLE
             progressPercent.visibility = View.INVISIBLE
         }
-        mHandler.post { dialog?.show() }
+        dialog?.show()
 
         if (uninstall) {
             if (!ShellUtils.isRootAvailable) {
--- app/src/main/java/com/brit/swiftinstaller/ui/activities/UninstallFinishedActivity.kt
+++ app/src/main/java/com/brit/swiftinstaller/ui/activities/UninstallFinishedActivity.kt
@@ -1,14 +1,12 @@
 package com.brit.swiftinstaller.ui.activities
 
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AlertDialog
 import com.brit.swiftinstaller.R
 
 class UninstallFinishedActivity : ThemeActivity() {
 
     lateinit var dialog: AlertDialog
-    private val mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +29,7 @@ class UninstallFinishedActivity : ThemeActivity() {
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
-            mHandler.post { dialog.show() }
+            dialog.show()
         }
     }
 
