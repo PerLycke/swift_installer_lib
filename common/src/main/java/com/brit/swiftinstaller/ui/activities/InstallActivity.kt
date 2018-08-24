05316c046a405997188f116323423ef07713855c
PerLycke <per.lycke@gmail.com>
2018-08-24 13:47:51 +0200

    Installer: remove unused import

--- common/src/main/java/com/brit/swiftinstaller/ui/activities/InstallActivity.kt
+++ common/src/main/java/com/brit/swiftinstaller/ui/activities/InstallActivity.kt
@@ -6,11 +6,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.os.Handler
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.brit.swiftinstaller.installer.Notifier
 import com.brit.swiftinstaller.installer.rom.RomInfo
 import com.brit.swiftinstaller.library.R
