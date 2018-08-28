82db1ba35258fa27411b9267c377a094a50b92e1
The Imp <theimp0417@gmail.com>
2018-08-29 03:22:18 +0530

    Add required overlays for Aosp and oos

--- app/src/main/java/com/brit/swiftinstaller/installer/rom/OOSRomInfo.kt
+++ app/src/main/java/com/brit/swiftinstaller/installer/rom/OOSRomInfo.kt
@@ -2,4 +2,34 @@ package com.brit.swiftinstaller.installer.rom
 
 import android.content.Context
 
-class OOSRomInfo(context: Context) : OreoRomInfo(context)
+class OOSRomInfo(context: Context) : OreoRomInfo(context) {
+
+    override fun getRequiredApps(): Array<String> {
+        return Array(21) {
+            when (it) {
+                0 -> "android"
+                1 -> "com.android.systemui"
+                2 -> "com.amazon.clouddrive.photos"
+                3 -> "com.android.settings"
+                4 -> "com.anydo"
+                5 -> "com.apple.android.music"
+                6 -> "com.ebay.mobile"
+                7 -> "com.embermitre.pixolor.app"
+                8 -> "com.google.android.apps.genie.geniewidget"
+                9 -> "com.google.android.apps.inbox"
+                10 -> "com.google.android.apps.messaging"
+                11 -> "com.google.android.gm"
+                12 -> "com.google.android.talk"
+                13 -> "com.mxtech.videoplayer.ad"
+                14 -> "com.mxtech.videoplayer.pro"
+                15 -> "com.pandora.android"
+                16 -> "com.simplecity.amp.pro"
+                17 -> "com.Slack"
+                18 -> "com.twitter.android"
+                19 -> "com.google.android.gms"
+                20 -> "de.axelspringer.yana.zeropage"
+                else -> ""
+            }
+        }
+    }
+}
--- app/src/main/java/com/brit/swiftinstaller/installer/rom/OreoRomInfo.kt
+++ app/src/main/java/com/brit/swiftinstaller/installer/rom/OreoRomInfo.kt
@@ -16,6 +16,35 @@ open class OreoRomInfo(context: Context) : RomInfo(context) {
         }
     }
 
+    override fun getRequiredApps(): Array<String> {
+        return Array(21) {
+            when (it) {
+                0 -> "android"
+                1 -> "com.android.systemui"
+                2 -> "com.amazon.clouddrive.photos"
+                3 -> "com.android.settings"
+                4 -> "com.anydo"
+                5 -> "com.apple.android.music"
+                6 -> "com.ebay.mobile"
+                7 -> "com.embermitre.pixolor.app"
+                8 -> "com.google.android.apps.genie.geniewidget"
+                9 -> "com.google.android.apps.inbox"
+                10 -> "com.google.android.apps.messaging"
+                11 -> "com.google.android.gm"
+                12 -> "com.google.android.talk"
+                13 -> "com.mxtech.videoplayer.ad"
+                14 -> "com.mxtech.videoplayer.pro"
+                15 -> "com.pandora.android"
+                16 -> "com.simplecity.amp.pro"
+                17 -> "com.Slack"
+                18 -> "com.twitter.android"
+                19 -> "com.google.android.gms"
+                20 -> "de.axelspringer.yana.zeropage"
+                else -> ""
+            }
+        }
+    }
+
     override fun postInstall(uninstall: Boolean, apps: ArrayList<String>, oppositeApps: ArrayList<String>?, intent: Intent?) {
         if (ShellUtils.isRootAvailable && !uninstall) {
             for (app in apps) {
